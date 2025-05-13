a2af160c6c360aca02240ee838324ee371b521c4
StreitLeak <streitleak@gmail.com>
2025-05-13 20:18:52 +0800

    Update RadiusCDRController.php

--- src/app/http/controllers/RadiusCDRController.php
+++ src/app/http/controllers/RadiusCDRController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Streitleak\RadiusWeb\app\models\CDR;
+use Streitleak\Radiusweb\app\models\CDR;
 use Illuminate\Support\Facades\Validator;
 use Carbon\Carbon;
 use DB;
